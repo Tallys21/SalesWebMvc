57e97d6b733840d78c26aa4a694b0a48f69668c4
Tallys21 <tallys-santos21@hotmail.com>
2024-12-10 19:17:53 -0300

    Seeding Service

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,14 +1,29 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using SalesWebMvc.Data;
 using SalesWebMvc.Models;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<SalesWebMvcContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("SalesWebMvcContext"), new MySqlServerVersion(new Version(2, 1, 1)), bb => bb.MigrationsAssembly("SalesWebMvc")));
 
+builder.Services.AddScoped<SeedingService>();
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+
+    using (var scope = app.Services.CreateScope())
+    {
+        var context = scope.ServiceProvider.GetRequiredService<SalesWebMvcContext>();
+        var seeding = scope.ServiceProvider.GetRequiredService<SeedingService>();
+
+        seeding.Seed();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
